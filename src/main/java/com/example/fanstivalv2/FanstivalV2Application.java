773f06969cc90943c2459a48f6eb4d935e0591ac
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-03-11 15:42:24 +0900

    first commit

---
+++ src/main/java/com/example/fanstivalv2/FanstivalV2Application.java
@@ -0,0 +1,13 @@
+package com.example.fanstivalv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanstivalV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FanstivalV2Application.class, args);
+    }
+
+}
