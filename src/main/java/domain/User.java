2a5fa2b195a7d9ab895d6c2b810292fc86f60109
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-03-11 17:33:45 +0900

    feat: User Entity 생성 및 연관 관계 매핑

---
+++ src/main/java/domain/User.java
@@ -0,0 +1,55 @@
+package domain;
+
+
+import domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.util.ArrayList;
+
+@Entity
+@Getter
+@Builder
+@DynamicUpdate
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickname;
+
+    private String loginType;
+
+    private String socialId;
+
+    private String profileImage;
+
+    @Column(columnDefinition = "TINYINT(1)")
+    @ColumnDefault("1")
+    private int status; //  0: 비활성화, 1: 활성
+
+    private Role role;//enum type으로 가수, 팬, 둘 다로 생성
+
+    private Grade grade;//enum type으로 등급 정하기
+
+    @Builder.Default
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<follower> followerList = new ArrayList<>();//follower로 좋아하는 가수 설정
+
+    public void setNickname(String nickname){
+        this.nickname = nickname;
+    }
+
+    public void setProfileImage(String profileImage){
+        this.profileImage = profileImage;
+    }
+
+    public void setStatus(int status){
+        this.status = status;
+    }
+}
