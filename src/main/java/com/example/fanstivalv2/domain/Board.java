a1a91547db295de640bbfaf06ef53affd8f1098a
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-04-01 20:28:50 +0900

    chore: Board Entity JoinColumName삭제

--- src/main/java/com/example/fanstivalv2/domain/Board.java
+++ src/main/java/com/example/fanstivalv2/domain/Board.java
@@ -20,7 +20,7 @@ public class Board extends BaseEntity {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    @JoinColumn(name = "user_id")
     private User user;
 
     private String title;
