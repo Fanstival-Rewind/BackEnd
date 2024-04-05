4968754c403068a26c578f7b4b8a4a99dd0a589e
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-04-05 15:17:18 +0900

    feat: OAuth2LoginFalilureHandler 코드 작성

--- src/main/java/com/example/fanstivalv2/ouath2/handler/OAuth2LoginFailureHandler.java
+++ src/main/java/com/example/fanstivalv2/ouath2/handler/OAuth2LoginFailureHandler.java
@@ -4,7 +4,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
