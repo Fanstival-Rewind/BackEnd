d02aefd99b3e713defa0fb38e0917c90fb03904b
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-04-10 02:07:52 +0900

    feat: getBoard BoardQueryService 작성

--- src/main/java/com/example/fanstivalv2/service/board/BoardQueryService.java
+++ src/main/java/com/example/fanstivalv2/service/board/BoardQueryService.java
@@ -1,4 +1,9 @@
 package com.example.fanstivalv2.service.board;
 
+import com.example.fanstivalv2.domain.Board;
+import jakarta.servlet.http.HttpServletRequest;
+
 public interface BoardQueryService {
+
+    Board getBoard(HttpServletRequest httpServletRequest, Long id);
 }
