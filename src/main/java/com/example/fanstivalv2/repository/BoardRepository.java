1695314fad0a713df438691f254b1946394ce472
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-04-09 15:37:14 +0900

    feat: BoardRepository findByIdAndUserId: boardId와 userId로 게시판 탐색 메서드 작성

--- src/main/java/com/example/fanstivalv2/repository/BoardRepository.java
+++ src/main/java/com/example/fanstivalv2/repository/BoardRepository.java
@@ -3,6 +3,9 @@ package com.example.fanstivalv2.repository;
 import com.example.fanstivalv2.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    
+
+    Optional<Board> findByIdAndUserId(Long boardId, Long userId);
 }
