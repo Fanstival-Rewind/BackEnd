6a4267458dd0a95772deee8749f3b71cb53d6903
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-04-11 02:33:25 +0900

    feat: JPA를 이용한 Repository findAllByUserId 코드 작성

--- src/main/java/com/example/fanstivalv2/repository/BoardRepository.java
+++ src/main/java/com/example/fanstivalv2/repository/BoardRepository.java
@@ -1,11 +1,15 @@
 package com.example.fanstivalv2.repository;
 
 import com.example.fanstivalv2.domain.Board;
+import com.example.fanstivalv2.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
     Optional<Board> findByIdAndUserId(Long boardId, Long userId);
+
+    List<Board> findAllByUserId(User user);
 }
