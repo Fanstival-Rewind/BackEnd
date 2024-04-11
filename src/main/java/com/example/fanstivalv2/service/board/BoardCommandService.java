288399bdbe37d6fa33825ecdb87f4eb6b1473563
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-04-12 01:53:18 +0900

    feat: Board Delete QueryService 코드 작성 -> JPA적용

--- src/main/java/com/example/fanstivalv2/service/board/BoardCommandService.java
+++ src/main/java/com/example/fanstivalv2/service/board/BoardCommandService.java
@@ -2,6 +2,7 @@ package com.example.fanstivalv2.service.board;
 
 import com.example.fanstivalv2.domain.Board;
 import com.example.fanstivalv2.web.dto.board.BoardRequestDto;
+import com.example.fanstivalv2.web.dto.board.BoardResponseDto;
 import jakarta.servlet.http.HttpServletRequest;
 
 public interface BoardCommandService {
@@ -9,4 +10,5 @@ public interface BoardCommandService {
     Board createBoard(HttpServletRequest httpServletRequest, BoardRequestDto.CreateBoardDto request);
     Board updateBoard(HttpServletRequest httpServletRequest, Long id, BoardRequestDto.UpdateBoardDto request);
 
+    BoardResponseDto.DeleteBoardResultDTO deleteBoard(HttpServletRequest httpServletRequest, Long id);
 }
